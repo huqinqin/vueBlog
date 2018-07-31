ec7f0212dd831ccdd96e97f10781b22ee5e753e4
DESKTOP-IBOCO5L\work <huqin@huntlee.cn>
2018-07-31 15:02:51 +0800

    first commit

---
+++ src/components/login/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="login">
+      <el-button>111</el-button>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/mock/index.js
@@ -0,0 +1,3 @@
+import Vue from 'vue'
+import Mock from 'mockjs'
+Vue.use(Mock)
